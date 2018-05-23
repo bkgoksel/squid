4691f40caa1d855500f4a45760030546eb8cf36c
Kerem Goksel <bkgoksel@gmail.com>
2018-05-22 18:34:54 -0700

    Finish data download script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -9,7 +9,7 @@ cl down train-v1.1.json
 cl down dev-v1.1.json
 cl down tiny-dev.json
 cl down train-rule-unans-weighted.json
-cl down dev-rule-unans.json
+l down dev-rule-unans.json
 cl down train-v2.0.json
 cl down dev-v2.0.json
 cl down generate-tfidf-train-2
@@ -18,23 +18,25 @@ cl down train-nosent-neg.json
 cl down dev-nosent-neg.json
 
 mkdir data/original
-mv train-v1.1.json original/
-mv dev-v1.1.json original/
+mv train-v1.1.json data/original/train.json
+mv dev-v1.1.json data/original/dev.json
+mv tiny-dev.json data/original/tiny-dev.json
 
 mkdir data/negatives
 
 mkdir data/negatives/rule
-mv train-rule-unans-weighted.json data/negatives/rule/
-mv dev-rule-unans.json data/negatives/rule/
+mv train-rule-unans-weighted.json data/negatives/rule/train.json
+mv dev-rule-unans.json data/negatives/rule/dev.json
 
 mkdir data/negatives/squadrun
-mv train-v2.0.json data/negatives/squadrun/
-mv dev-v2.0.json data/negatives/squadrun/
+mv train-v2.0.json data/negatives/squadrun/train.json
+mv dev-v2.0.json data/negatives/squadrun/dev.json
 
 mkdir data/negatives/tfidf
-mv generate-tfidf-train-2 data/negatives/tfidf/
-mv squad_dev_tfidf_n2.json data/negatives/tfidf/
+mv generate-tfidf-train-2/tfidf.json data/negatives/tfidf/train.json
+mv squad_dev_tfidf_n2.json data/negatives/tfidf/dev.json
+rm -rf generate-tfidf-train-2
 
 mkdir data/negatives/nosent
-mv train-nosent-neg.json data/negatives/nosent/
-mv dev-nosent-neg.json data/negatives/nosent/
+mv train-nosent-neg.json data/negatives/nosent/train.json
+mv dev-nosent-neg.json data/negatives/nosent/dev.json
